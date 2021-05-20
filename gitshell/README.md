a69c614eb6d78c476ef98761c4f987508e598416
Shastick <julien.perro@gmail.com>
2021-05-20 10:56:52 +0200

    move gitshell dependency to the public go libraries
    GitOrigin-RevId: 6b69fab2eb67f0405d7011e87fb38c441a4baf04

---
+++ gitshell/README.md
@@ -0,0 +1,5 @@
+# GitShell
+
+Minimal go library to execute git commands from the shell.
+
+Obviously requires to have `git` on your `PATH`.
---
+++ gitshell/gitshell.go
@@ -0,0 +1,68 @@
+package gitshell
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+// GitResolveRevision prints the SHA1 hash given a revision specifier
+// see https://git-scm.com/docs/git-rev-parse for more details
+func GitResolveRevision(inPath string, branch string) string {
+	var (
+		cmdOut []byte
+		err    error
+	)
+    if cmdOut, err = exec.Command("git","-C", inPath, "rev-parse", branch).Output(); err != nil {
+        fmt.Fprintln(os.Stderr, "There was an error running the git rev-parse command: ", err)
+        os.Exit(1)
+    }
+    sha := string(cmdOut)
+    return sha[0:40]
+}
+
+// GitAdd adds a change in the working directory to the staging area
+// see https://git-scm.com/docs/git-add for more details
+func GitAdd(inPath string, filePath string ){
+	if err := exec.Command("git", "-C", inPath, "add", filePath).Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println("Successfully added File")
+}
+
+// GitCommit saves your changes to the local repository
+// see https://git-scm.com/docs/git-commit for more details
+func GitCommit(inPath string, commitMsg string){
+	if err := exec.Command("git","-C", inPath, "commit","-m", commitMsg).Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println("Successfully committed with given commit message")
+}
+
+// GitCommitMessageFromHash returns the commit message from the given commit hash
+// see https://git-scm.com/docs/git-log for more details
+func GitCommitMessageFromHash(inPath string, hash string) string{
+	var (
+		cmdOut []byte
+		err    error
+	)
+    if cmdOut, err = exec.Command("git","-C", inPath, "log", "-n", "1", "--pretty=format:%B", hash).Output(); err != nil {
+        fmt.Fprintln(os.Stderr, "There was an error returning the git commit message: ", err)
+        os.Exit(1)
+    }
+    message := string(cmdOut)
+    return message
+
+}
+
+// GitCheckout lets you navigate between the branches
+// see https://git-scm.com/docs/git-checkout for more details
+func GitCheckout(inPath string, commitOrBranch string ){
+	if err := exec.Command("git", "-C", inPath, "checkout", commitOrBranch).Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println("Checkout out branch or commit")
+}
